e36ba465d40f0975b085d575813c4d490449be78
蔡瑶 <caiyao@caiyaodemac-mini.local>
2016-04-27 14:54:34 +0800

    Initial Commit

---
+++ BLSetting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLSetting
+//
+//  Created by 蔡瑶 on 16/4/27.
+//  Copyright © 2016年 bailun91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BLSetting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLSetting
+//
+//  Created by 蔡瑶 on 16/4/27.
+//  Copyright © 2016年 bailun91. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
